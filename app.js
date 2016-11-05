19745b9f9b3be93c9dcc08dd7cdeb50b0f3ce259
butaosuinu <buta.osuinu@gmail.com>
2016-11-05 14:59:45 +0900

    add https server

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-"use strict"
+'use strict'
 
 const koa = require('koa')
 const cors = require('kcors')
 const serve = require('koa-static')
+const https = require('https')
+const http = require('http')
+const fs = require('fs')
 const app = koa()
+const options = {
+	key: fs.readFileSync('keys/decrypt_server.key'),
+	cert: fs.readFileSync('keys/server.crt')
+}
 
 const origins = {
   origin: '*',
@@ -12,15 +19,22 @@ const origins = {
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With']
 }
 
+const port = 8080
+const securePort = 4430
+
 app.use(cors(origins))
 app.use(serve(__dirname + '/public'));
 
-app.listen(8080, function() {
-	console.log('Server start');
-})
 
-app.on('error', function(err) {
+app.on('error', function (err) {
 	log.error('server error', err);
 });
 
 const router = require('./controller/routes.js')(app)
+
+https.createServer(options, app.callback()).listen(securePort, function () {
+	console.log('Secure Server start')
+})
+http.createServer(app.callback()).listen(port, function () {
+	console.log('Secure Server start')
+})

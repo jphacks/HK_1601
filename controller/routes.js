2259cfebde1cc7c7815a217fa5dbf84d28b4910f
butaosuinu <buta.osuinu@gmail.com>
2016-11-05 13:45:53 +0900

    wip apis

--- controller/routes.js
+++ controller/routes.js
@@ -1,19 +1,41 @@
 "strict mode"
 
-const Router = require('koa-router')()
-const json = require('koa-json')
+const Router     = require('koa-router')()
+const json       = require('koa-json')
 
-const homeController = require('./home.js')
+const home       = require('./home.js')
+const voice      = require('./voice.js')
+const motion     = require('./motion.js')
+const face       = require('./face.js')
+const statistics = require('./statistics.js')
 
 
 const route = (app, render) => {
 	app.use(json())
 
-	Router.get('/', homeController.getHome)
+	const APIv1 = new Router({
+		prefix: '/api/v1'
+	})
+
+	APIv1
+		.get('/voice', )
+		.post('/voice', )
+
+		// .get('/motion')
+		// .post('/motion')
+
+		// .get('/face')
+		// .post('/face')
+
+	Router
+		.get('/', home.getHome)
+		// .get('statistics')
 
 	app
 		.use(Router.routes())
-		.use(Router.allowedMethods());
+		.use(Router.allowedMethods())
+		.use(APIv1.routes())
+		.use(APIv1.allowedMethods())
 }
 
 module.exports = route

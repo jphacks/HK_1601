816a86af5265b81fb7a2054015d855cb46de4d2f
DSTLC-V.S.I.N <dstlc.vsin@gmail.com>
2016-11-07 08:09:34 +0900

    add Face Client
    追加し忘れた表情認識クライアントのソースコードです。

---
+++ face client/Emotion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceTest
+{
+    class Emotion
+    {
+        internal async Task<string> SendPictureAsync(Stream image)
+        {
+            WebRequest req = WebRequest.Create("https://api.projectoxford.ai/emotion/v1.0/recognize");
+            //メソッドにPOSTを指定
+            req.Method = "POST";
+            req.ContentType = "application/octet-stream";
+            req.ContentLength = image.Length;
+            req.Headers.Add("Ocp-Apim-Subscription-Key", "2788fce8a4b945e89c5be5a61e996791");
+
+            using (var rs = await req.GetRequestStreamAsync())
+            {
+                await image.CopyToAsync(rs);
+
+                //サーバーからの応答を受信するためのWebResponseを取得
+                using (var res = await req.GetResponseAsync())
+                {
+                    //応答データを受信するためのStreamを取得
+                    Stream resStream = res.GetResponseStream();
+                    //受信して返却
+                    using (var sr = new StreamReader(resStream))
+                    {
+                        return await sr.ReadToEndAsync();
+                    }
+                }
+            }
+        }
+
+        internal async Task SendResultServer(string json)
+        {
+            var data = Encoding.ASCII.GetBytes(json);
+            WebRequest req = WebRequest.Create("https://localhost/api/v1/face");
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.ContentLength = data.Length;
+
+            using (var rs = await req.GetRequestStreamAsync())
+            {
+                await rs.WriteAsync(data, 0, data.Length);
+
+                //サーバーからの応答を受信するためのWebResponseを取得
+                using (var res = await req.GetResponseAsync())
+                {
+                }
+            }
+        }
+    }
+}
---
+++ face client/Program.cs
@@ -0,0 +1,68 @@
+﻿using OpenCvSharp;
+using System;
+using System.IO;
+
+namespace FaceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emotion = new Emotion();
+
+            //CreateCameraCaptureの引数はカメラのIndex(通常は0から始まる)
+            Restart:
+            using (var capture = Cv.CreateCameraCapture(0))
+            {
+                IplImage frame;// = new IplImage();
+
+                //  W320 x H240のウィンドウを作る
+                double w = 320, h = 240;
+                Cv.SetCaptureProperty(capture, CaptureProperty.FrameWidth, w);
+                Cv.SetCaptureProperty(capture, CaptureProperty.FrameHeight, h);
+
+                //  何かキーを押すまでは、Webカメラの画像を表示し続ける
+                //Task.Delay(500).Wait();
+                Console.WriteLine("いずれかのキーを入力すると終了します。\n");
+
+                NextScan:
+                //  カメラからフレームを取得
+                int miss = 0;
+                while ((frame = Cv.QueryFrame(capture)) == null)
+                {
+                    if (Console.KeyAvailable)
+                        return;
+                    //Task.Delay(500).Wait();
+                    if (++miss > 2)
+                        goto Restart;
+                }
+
+                //  Window「Capture」を作って、Webカメラの画像を表示
+                Cv.ShowImage("Capture", frame);
+
+                string face;
+                using (var image = new MemoryStream())
+                {
+                    frame.ToStream(image, ".png");
+                    image.Position = 0L;
+
+                    face = emotion.SendPictureAsync(image).Result;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(face.Replace(",", ",\n").Replace("{","{\n"));
+                //閉じる
+                if (Cv.WaitKey(5000) == -1 && !Console.KeyAvailable)
+                {
+                    goto NextScan;
+                }
+
+                //  bmp以外にも、jpegやpngでの保存が可能
+                //frame.SaveImage("result.bmp");
+
+                //  使い終わったWindow「Capture」を破棄
+                Cv.DestroyWindow("Capture");
+            }
+        }
+    }
+}
